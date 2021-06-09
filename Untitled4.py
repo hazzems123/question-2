09bb941768d85ff7ad7c437eff3dce694825254d
hazzems123 <hazzems@hotmail.com>
2021-06-09 17:52:58 +0200

    Add files via upload

---
+++ Untitled4.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+n = 5
+
+def fact (n) :
+    if n ==1 :
+        return n
+    else:
+        return n * fact (n-1)
+
+
+# In[9]:
+
+
+x = fact (n)
+
+
+# In[10]:
+
+
+print (x)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
